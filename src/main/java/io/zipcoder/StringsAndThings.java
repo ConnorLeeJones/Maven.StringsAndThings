e5b4c7b4602cae2816b0ab353459d35f02a4dffe
Connor Jones <ConnorLeeJones@gmail.com>
2019-05-30 17:06:43 -0400

    Finished

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -1,5 +1,8 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
  * @author tariq
@@ -15,7 +18,16 @@ public class StringsAndThings {
      *           countYZ("day fyyyz"); // Should return 2
      */
     public Integer countYZ(String input){
-        return null;
+            String[] sentenceArray = input.split(" ");
+            int count = 0;
+
+            for (String element : sentenceArray){
+                if (element.charAt(element.length() - 1) == 'y' || element.charAt(element.length() - 1) == 'z'){
+                      count++;
+                }
+            }
+        System.out.println(sentenceArray.toString());
+        return count;
     }
 
     /**
@@ -28,7 +40,8 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+        base = base.replace(remove, "");
+        return base;
     }
 
     /**
@@ -39,9 +52,33 @@ public class StringsAndThings {
      *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
      *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
-    public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
+    public Boolean containsEqualNumberOfIsAndNot(String input) {
+        String is = "is";
+        String not = "not";
+
+        int isNumber = 0;
+        int notNumber = 0;
+
+        Pattern pattern = Pattern.compile(is);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            isNumber++;
+        }
+
+        pattern = Pattern.compile(not);
+        matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            notNumber++;
+        }
+
+        return notNumber == isNumber;
+
     }
+
+
+
 
     /**
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
@@ -50,8 +87,20 @@ public class StringsAndThings {
      *           gHappy("xxgxx") // Should return  false
      *           gHappy("xxggyygxx") // Should return  false
      */
-    public Boolean gIsHappy(String input){
-        return null;
+    public Boolean gIsHappy(String input) {
+        //String[] stringArray = input;
+
+        boolean boul = true;
+
+        for (int i = 1; i < input.length() - 1; i++) {
+            if (input.charAt(i) == 'g') {
+                if (!((input.charAt(i - 1) == 'g') || (input.charAt(i + 1) == 'g'))) {
+                    boul = false;
+                    break;
+                }
+            }
+        }
+            return boul;
     }
 
 
@@ -63,6 +112,14 @@ public class StringsAndThings {
      *            countTriple("a") // Should return 0
      */
     public Integer countTriple(String input){
-        return null;
+        int trips = 0;
+        String[] inputArr = input.split("");
+
+        for (int i = 0; i < inputArr.length - 2; i++){
+            if (inputArr[i].equals(inputArr[i+1]) && inputArr[i].equals(inputArr[i+2]))
+                trips++;
+        }
+
+        return trips;
     }
 }
